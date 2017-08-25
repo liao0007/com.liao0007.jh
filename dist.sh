4c7dc62fb4f038e7a60478e6df6fd52a1910ac7b
liaoliang <liao0007@gmail.com>
2017-08-25 22:52:51 +0800

    up

--- dist.sh
+++ dist.sh
@@ -8,6 +8,8 @@ sbt dist
 ./dist-play-app-initd.sh stop
 unzip -o ./target/universal/com-liao0007-jh-1.0-SNAPSHOT.zip -d /var/www/
 rm ./target/universal/com-liao0007-jh-1.0-SNAPSHOT.zip
+cp -rdf ./shell /var/www/com-liao0007-jh-1.0-SNAPSHOT/
+chmod -R +x /var/www/com-liao0007-jh-1.0-SNAPSHOT/shell
 
 # restart
 ./dist-play-app-initd.sh start
