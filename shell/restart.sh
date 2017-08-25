b4fe028c439cab8269b681ee7f069d3511d05bd6
liaoliang <liao0007@gmail.com>
2017-08-25 23:25:25 +0800

    up

--- shell/restart.sh
+++ shell/restart.sh
@@ -11,8 +11,8 @@ dontstarve_dir="$HOME/.klei/DoNotStarveTogether"
 $steamcmd_dir/steamcmd.sh +force_install_dir "$install_dir" +login anonymous +app_update 343050 validate +quit
 ssh root@lab.youleligou.com $steamcmd_dir/steamcmd.sh +force_install_dir "$install_dir" +login anonymous +app_update 343050 validate +quit
 
-$1/shell/sync_mod_config.sh
-$1/shell/sync_world_config.sh
+$1/shell/sync_mod_config.sh $1
+$1/shell/sync_world_config.sh $1
 
 screen -S dst -p 0 -X quit
 screen -S dst -d -m /root/run_dedicated_servers.sh
