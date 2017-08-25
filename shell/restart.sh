dd03d55df4594c134b0bce24e9c9b8a4e261612e
liaoliang <liao0007@gmail.com>
2017-08-25 23:23:29 +0800

    up

--- shell/restart.sh
+++ shell/restart.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+# $1 = play environment root path
+
 steamcmd_dir="$HOME/steamcmd"
 install_dir="$HOME/dontstarvetogether_dedicated_server"
 cluster_name="MyDediServer"
@@ -8,8 +11,8 @@ dontstarve_dir="$HOME/.klei/DoNotStarveTogether"
 $steamcmd_dir/steamcmd.sh +force_install_dir "$install_dir" +login anonymous +app_update 343050 validate +quit
 ssh root@lab.youleligou.com $steamcmd_dir/steamcmd.sh +force_install_dir "$install_dir" +login anonymous +app_update 343050 validate +quit
 
-./sync_mod_config.sh
-./sync_world_config.sh
+$1/shell/sync_mod_config.sh
+$1/shell/sync_world_config.sh
 
 screen -S dst -p 0 -X quit
 screen -S dst -d -m /root/run_dedicated_servers.sh
