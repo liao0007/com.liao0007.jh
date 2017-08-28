1629022b44bcc94b0a0a8167008ed1675c358e78
liaoliang <liao0007@gmail.com>
2017-08-28 17:00:28 +0800

    up

--- dist-play-app-initd.sh
+++ dist-play-app-initd.sh
@@ -76,7 +76,7 @@ COMMAND=$1
 # *************  Set these variables  ***********
 
 NAME=com-liao0007-jh
-PORT=9001
+PORT=9009
 APP_DIR=/var/www/com-liao0007-jh-1.0-SNAPSHOT
 
 # ***********************************************
